51af85acfed5640bf308c6e191de936fb5e5f7ec
Dawid Gajda <dawid.gajda3@gmail.com>
2022-04-20 14:26:41 +0200

    Refactoring password verification and tests

--- password_verification.py
+++ password_verification.py
@@ -1,3 +1,13 @@
+"""Password verification
+Min eight length
+Min one number
+Min one upper letter
+Min one lower letter
+Min one special char
+
+    Returns:
+        Boolean: True when meets standard or else False
+    """
 from string import punctuation
 
 PASSWORD_LENGTH = 8
@@ -6,44 +16,80 @@ SPECIAL_CHAR = list(punctuation)
 
 
 class PasswordVerification:
+    """Password verification"""
+
     def __init__(self, password) -> None:
         self.password = password
         self.password_standard = True
+        self.check_values = []
 
-    def change_flag_password_standard(self):
-        self.password_standard = False
+    def check_len(self):
+        """Check length password
+
+        Returns:
+            Boolean: True when meets standard or else False
+        """
+        if len(self.password) < PASSWORD_LENGTH:
+            return not self.password_standard
         return self.password_standard
+
+    def check_is_number(self):
+        """Check is number in password
+
+        Returns:
+            Boolean: True when meets standard or else False
+        """
+        for char in self.password:
+            if char in NUMBERS:
+                return self.password_standard
+        return not self.password_standard
+
+    def check_is_upper_letter(self):
+        """Check is upper letter in password
+
+        Returns:
+            Boolean: True when meets standard or else False
+        """
+        for char in self.password:
+            if char.isupper():
+                return self.password_standard
+        return not self.password_standard
+
+    def check_is_lower_letter(self):
+        """Check is lower letter in password
+
+        Returns:
+            Boolean: True when meets standard or else False
+        """
+        for char in self.password:
+            if char.islower():
+                return self.password_standard
+        return not self.password_standard
+
+    def check_is_special_char(self):
+        """Check is special char in password
+
+        Returns:
+            Boolean: True when meets standard or else False
+        """
+        for char in self.password:
+            if char in SPECIAL_CHAR:
+                return self.password_standard
+        return not self.password_standard
 
     def check_password(self):
-        if len(self.password) < PASSWORD_LENGTH:
-            self.change_flag_password_standard()
+        """Check all standard in password
 
-        is_number = 0
-        for char in self.password:
-            if char not in NUMBERS:
-                is_number += 1
-            if is_number == len(self.password):
-                self.change_flag_password_standard()
+        Returns:
+            Boolean: True when meets standard or else False
+        """
+        self.check_values.append(self.check_len())
+        self.check_values.append(self.check_is_number())
+        self.check_values.append(self.check_is_upper_letter())
+        self.check_values.append(self.check_is_lower_letter())
+        self.check_values.append(self.check_is_special_char())
+        print(self.check_values)
 
-        is_upper = 0
-        for char in self.password:
-            if not char.isupper():
-                is_upper += 1
-            if is_upper == len(self.password):
-                self.change_flag_password_standard()
-
-        is_lower = 0
-        for char in self.password:
-            if not char.islower():
-                is_lower += 1
-            if is_lower == len(self.password):
-                self.change_flag_password_standard()
-
-        is_special_char = 0
-        for char in self.password:
-            if char not in SPECIAL_CHAR:
-                is_special_char += 1
-            if is_special_char == len(self.password):
-                self.change_flag_password_standard()
-
-        return self.password_standard
+        if all(self.check_values):
+            return True
+        return False
--- password_verification_test.py
+++ password_verification_test.py
@@ -2,36 +2,45 @@ from password_verification import PasswordVerification
 
 
 def test_len_password():
-    password = 'Admin1!'
-    password_verification = PasswordVerification(password)
-    assert password_verification.check_password() == False
+    password = PasswordVerification('AdminAdmin')
+    password_short = PasswordVerification('Admin')
+    assert password.check_len() == True
+    assert password_short.check_len() == False
 
 
 def test_number_in_password():
-    password = 'AdminAdmin!'
-    password_verification = PasswordVerification(password)
-    assert password_verification.check_password() == False
+    is_number = PasswordVerification('Admin1')
+    not_is_number = PasswordVerification('Admin')
+    assert is_number.check_is_number() == True
+    assert not_is_number.check_is_number() == False
 
 
 def test_upper_letter_in_password():
-    password = 'adminadmin1!'
-    password_verification = PasswordVerification(password)
-    assert password_verification.check_password() == False
+    is_upper = PasswordVerification('adminA')
+    not_is_upper = PasswordVerification('admin')
+    assert is_upper.check_is_upper_letter() == True
+    assert not_is_upper.check_is_upper_letter() == False
 
 
 def test_lower_letter_in_password():
-    password = 'ADMINADMIN1!'
-    password_verification = PasswordVerification(password)
-    assert password_verification.check_password() == False
+    is_lower = PasswordVerification('ADMINa')
+    not_is_lower = PasswordVerification('ADMIN')
+    assert is_lower.check_is_lower_letter() == True
+    assert not_is_lower.check_is_lower_letter() == False
 
 
 def test_special_char_in_password():
-    password = 'AdminAdmin1'
-    password_verification = PasswordVerification(password)
-    assert password_verification.check_password() == False
+    is_special_char = PasswordVerification('Admin!')
+    not_is_special_char = PasswordVerification('Admin')
+    assert is_special_char.check_is_special_char() == True
+    assert not_is_special_char.check_is_special_char() == False
 
 
-def test_password_standard():
-    password = 'Admin1!Admin2@'
-    password_verification = PasswordVerification(password)
-    assert password_verification.check_password() == True
+def test_check_password():
+    is_standard = PasswordVerification('Admin1@#')
+    assert is_standard.check_password() == True
+    assert PasswordVerification('Admin1@').check_password() == False
+    assert PasswordVerification('AdminAdmin@').check_password() == False
+    assert PasswordVerification('admin1@a').check_password() == False
+    assert PasswordVerification('ADMIN1@A').check_password() == False
+    assert PasswordVerification('Admin1admin').check_password() == False
